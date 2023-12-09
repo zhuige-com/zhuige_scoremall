377c4a7f386c621f63e1a70d089f9ae336b5bc86
longwenjunjie <longwenjunj@163.com>
2023-12-09 11:32:17 +0800

    ## 1.3.9（2023-12-09） 1. 新增更新功能

--- client/App.vue
+++ client/App.vue
@@ -1,6 +1,6 @@
 <script>
 	/*
-	 * 追格积分商城小程序 v1.3.8
+	 * 追格积分商城小程序 v1.3.9
 	 * 作者: 追格
 	 * 文档: https://www.zhuige.com/docs/jf
 	 * gitee: https://gitee.com/zhuige_com/zhuige_scoremall
@@ -21,6 +21,27 @@
 				success: (e) => {
 					Vue.prototype.$is_ios = (e.platform == 'ios');
 				}
+			})
+			
+			const updateManager = wx.getUpdateManager()
+			updateManager.onCheckForUpdate(function(res) {
+				// 请求完新版本信息的回调
+				// console.log(res.hasUpdate)
+			})
+			updateManager.onUpdateReady(function() {
+				wx.showModal({
+					title: '更新提示',
+					content: '新版本已经准备好，是否重启应用？',
+					success: function(res) {
+						if (res.confirm) {
+							// 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+							updateManager.applyUpdate()
+						}
+					}
+				})
+			})
+			updateManager.onUpdateFailed(function() {
+				// 新版本下载失败
 			})
 		},
 
--- client/utils/constants.js
+++ client/utils/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
-	JQ_VERSION: '1.3.8',
+	JQ_VERSION: '1.3.9',
 
 	//分页 每页 数量 需要与服务器一致
 	JQ_PER_PAGE_COUNT: 10,
